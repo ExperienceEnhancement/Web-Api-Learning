31a9483ba16663fe99c58db2959386788e507ce4
GBurlakova <gabriela.burlakova@gmail.com>
2016-07-22 16:06:40 +0300

    Added Web API starter project

---
+++ Web-Api-Starter-Project/Web-Api-Starter-Project/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Web_Api_Starter_Project.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Web-Api-Starter-Project/Web-Api-Starter-Project/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace Web_Api_Starter_Project.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
